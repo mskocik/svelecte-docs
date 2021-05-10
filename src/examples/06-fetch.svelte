8f7b0e582bba9b2cdc1d4914810732e239ce07b9
Martin Skočík <mskocik@gmail.com>
2021-05-10 11:10:49 +0200

    reflect changes in v1.3.0

--- src/examples/06-fetch.svelte
+++ src/examples/06-fetch.svelte
@@ -1,8 +1,22 @@
 <script>
   import Svelecte from 'svelecte';
+
+  let resetOnBlur = true;
+  let fetchResetOnBlur = true;
 </script>
 
 <Svelecte
+  {resetOnBlur}
+  {fetchResetOnBlur}
+  multiple
   placeholder="Start typing ('re' for example)"
   fetch="https://my-json-server.typicode.com/mskocik/svelecte-docs/colors?value_like=[query]"
 ></Svelecte>
+<div class="text-right">
+  <label for="rob" class="mr-2">
+    <input type="checkbox" name="" id="rob" bind:checked={resetOnBlur}> Reset on blur
+  </label>
+  <label for="frob">
+    <input type="checkbox" name="" id="frob" bind:checked={fetchResetOnBlur}> Reset search results on empty input
+  </label>
+</div>
--- src/examples/09-custom-dependent.svelte
+++ src/examples/09-custom-dependent.svelte
@@ -4,7 +4,13 @@
   function onSubmit(e) {
     const object = {};
     const formData = new FormData(e.target);
-    formData.forEach((value, key) => object[key] = value);
+    formData.forEach((value, key) => {
+      if (object[key]) {
+        object[key] += ', ' + value;
+        return;
+      }
+      object[key] = value
+    });
     payload = JSON.stringify(object, null, 2);
   }
 </script>
@@ -12,14 +18,23 @@
 
 <form action="" on:submit|preventDefault={onSubmit}>
   <el-svelecte
-    name="parent_value"
+    name="parent_value" placeholder="Select parent value"
     options={`[{"value":"posts","text":"Posts"},{"value":"users","text":"Users"},{"value":"comments","text":"Comments"}]`}
     id="is-parent" required>
   </el-svelecte>
-  <el-svelecte name="child_value" parent="is-parent" required
+  <el-svelecte name="child_value" parent="is-parent" required placeholder="Pick from child select"
     fetch="https://jsonplaceholder.typicode.com/[parent]">
   </el-svelecte>
-  <button type="submit" class="btn btn-success">Send form</button>
+  <!-- server-side rendered -->
+  <el-svelecte name="demo[]" options={`[{"value":"posts","text":"Posts"},{"value":"users","text":"Users"},{"value":"comments","text":"Comments"}]`} multiple
+    style="margin-bottom: 0"
+  >
+    <select id="anchored" name="demo[]" multiple on:change={e => console.log(e.target.selectedOptions)}></select>
+  </el-svelecte>
+  <small>This <code>&lt;el-svelecte&gt;</code> has nested (anchored) <code>&lt;select&gt;</code>, when you need to have it rendered server-side.</small>
+  <div class="mt-2">
+    <button type="submit" class="btn btn-success">Send form</button>
+  </div>
   {#if payload}
     <pre>{payload}</pre>
   {/if}
