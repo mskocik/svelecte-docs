572b3a416ec07a96cd95faa1da2f67890538f296
Martin Skočík <mskocik@gmail.com>
2021-05-10 13:13:18 +0200

    update, v1.3.1

--- src/examples/06-fetch.svelte
+++ src/examples/06-fetch.svelte
@@ -3,20 +3,26 @@
 
   let resetOnBlur = true;
   let fetchResetOnBlur = true;
+  let selectionWork = null;
 </script>
 
 <Svelecte
   {resetOnBlur}
   {fetchResetOnBlur}
+  bind:selection={selectionWork}
   multiple
   placeholder="Start typing ('re' for example)"
-  fetch="https://my-json-server.typicode.com/mskocik/svelecte-docs/colors?value_like=[query]"
+  fetch="https://jsonplaceholder.typicode.com/users/?email_like=[query]"
 ></Svelecte>
-<div class="text-right">
-  <label for="rob" class="mr-2">
-    <input type="checkbox" name="" id="rob" bind:checked={resetOnBlur}> Reset on blur
-  </label>
-  <label for="frob">
-    <input type="checkbox" name="" id="frob" bind:checked={fetchResetOnBlur}> Reset search results on empty input
-  </label>
+<div>
+  <b>Selected:</b>
+  <span class="float-right">
+    <label for="rob" class="mr-2">
+      <input type="checkbox" name="" id="rob" bind:checked={resetOnBlur}> Reset on blur
+    </label>
+    <label for="frob">
+      <input type="checkbox" name="" id="frob" bind:checked={fetchResetOnBlur}> Reset search results on empty input
+    </label>
+  </span>
+  <pre class="mt-1">{ JSON.stringify(selectionWork) }</pre>
 </div>
