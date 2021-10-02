c14216b92079ff7103e4e2cf7ebf2c1c41ec3e7a
Martin Skočík <mskocik@gmail.com>
2021-10-02 13:31:06 +0200

    update svelecte to 1.4.1, add example of remote for CE

--- src/docs.js
+++ src/docs.js
@@ -8,9 +8,10 @@ import Fetch from './examples/06-fetch.svelte';
 import Playground from './examples/07-playground.svelte';
 import Element from './examples/08-custom-element.svelte';
 import Dependent from './examples/09-custom-dependent.svelte';
-import Vue from './examples/10-vue.svelte';
+import Remote from './examples/10-custom-remote.svelte';
+import Vue from './examples/11-vue.svelte';
 
-[BasicExample, BasicExampleExt, Groups, ItemRender, Slot, Fetch, Playground, Element, Dependent, Vue]
+[BasicExample, BasicExampleExt, Groups, ItemRender, Slot, Fetch, Playground, Element, Dependent, Remote, Vue]
   .forEach(
     (component, index) => new component({
       target: document.getElementById(`example-${index +1}`),
